19940f5f9eacffdc6acbfece0eda17d6b9b00ed2
McEnery Peter <pmcenery@lcs-mba3012.lcs.on.ca>
2018-05-15 21:51:49 -0400

    Fixed up happy or sad

--- HappyOrSad/main.swift
+++ HappyOrSad/main.swift
@@ -8,41 +8,72 @@
 
 import Foundation
 
+//Input
+//make an error message for bad input
+let errorMessage = "Please enter a string that has at least 1 and no more than 255 characters."
+
+//prompt
+print("What did you say?")
+// Loop forever until valid input found
+var validInput = ""
+while true {
+    
+    // Unwrap the given input from user, make sure it is not nil
+    guard let givenInput = readLine() else {
+        
+        // Report the error then continue to next iteration of loop to prompt again
+        print(errorMessage)
+        continue
+        
+    }
+    
+    // Show the error message if input is not in range
+    if givenInput.count < 1 || givenInput.count > 255 {
+        
+        //prompt again
+        print(errorMessage)
+        continue
+        
+    }
+    
+    // If we got this far, input is valid
+    validInput = givenInput
+    break   // exit the loop
+    
+}
+
 // Get the user input
 var happyCount = 0
 var sadCount = 0
-var rawInput = readLine()
-
-// Print out the input provided
-print("You said:")
-print(rawInput)
-
-//make sure input is not nil
-// (unwrap the optional)
-guard let input = rawInput else {
-    //If we got here, input is nil, so quit program
-    exit(9) // exit with error code 9
-}
-
-//Print the verified input
-print(input)
 
 //Loop over each character of the input string
-for individualCharacter in input {
+for individualCharacter in validInput {
     
     //debug: what character are we looking at now
     print(individualCharacter)
     
     //is the character happy, sad, or neither
-    if individualCharacter == "😃" || individualCharacter == "🙂" || individualCharacter == "😊" || individualCharacter == "😄" {
+    switch individualCharacter {
+    case "😃", "🙂", "😊", "😄":
         happyCount += 1
-    } else if individualCharacter == "😔" || individualCharacter == "" {
+    case "😔", "☹", "🙁", "😕":
         sadCount += 1
-        print(happyCount)
+    default:
+        break //do nothing
     }
+   
 }
 
 // Outpuut
-
+//determine the emotion from the entered text
+    if happyCount == 0 && sadCount == 0 {
+        print("none")
+    } else if happyCount == sadCount {
+        print("unsure")
+    } else if happyCount > sadCount {
+        print("happy")
+    } else {
+        print("sad")
+}
 
 
